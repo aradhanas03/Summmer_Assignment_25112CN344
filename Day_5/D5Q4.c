ba3f124e24cc17847750237b9977ed5a30e39934
Aradhana Singh <aradhanas1627@gmail.com>
2026-06-05 02:20:44 +0530

    D5Q4.c

--- Day_5/D5Q4.c
+++ Day_5/D5Q4.c
@@ -1,30 +1,21 @@
 /*Write a program to Find largest prime factor.*/
 
 #include <stdio.h>
- int main() {
-    int n, i, j, isprime;
-    int largestprime = -1;
 
-    printf("enter a number : ");
+int main() {
+    int n, i, largestPrime = -1;
+
+    printf("Enter a number: ");
     scanf("%d", &n);
 
-    for(i=2; i<=n; i++) {
-        if(n % i == 0) {
-            isprime = 1;
-
-            for(j=2; j<= i/2; j++) {
-                if(i % j == 0){
-                    isprime = 0;
-                    break;
-                }
-            }
-
-            if(isprime) {
-                largestprime = i;
-            }
-        }    
+    for(i = 2; i <= n; i++) {
+        while(n % i == 0) {
+            largestPrime = i;
+            n = n / i;
+        }
     }
 
-    printf("largest prime factor %d is : ", largestprime);
+    printf("Largest Prime Factor = %d", largestPrime);
+
     return 0;
- }
+}
