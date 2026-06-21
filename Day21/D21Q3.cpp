e150711cfa1daaddd19354fb1e4ea87ebcac8be9
aradhanas03 <aradhanas1627@gmail.com>
2026-06-21 22:11:50 +0530

    assignment uploaded

---
+++ Day21/D21Q3.cpp
@@ -0,0 +1,30 @@
+/*Write a program to Count vowels and consonants.*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string str;
+    int vowels = 0, consonants = 0;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (char ch : str) {
+        ch = tolower(ch);
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    cout << "Vowels = " << vowels << endl;
+    cout << "Consonants = " << consonants << endl;
+
+    return 0;
+}
---
+++ Day21/D21Q4.cpp
@@ -0,0 +1,21 @@
+/*Write a program to Convert lowercase to uppercase.*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); i++) {
+        str[i] = toupper(str[i]);
+    }
+
+    cout << "Uppercase string: " << str << endl;
+
+    return 0;
+}
